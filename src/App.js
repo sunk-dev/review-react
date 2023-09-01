68eda090b08b5f8eb0a235801162ebd437665279
seon <sunkyoung.dev@gmail.com>
2023-09-01 17:16:20 +0900

    props 기초 , 객체 디스트럭쳐링

--- src/App.js
+++ src/App.js
@@ -3,21 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 import AppItem from './AppItem'; //APPItem 컴포넌트 불러오기
-import NoName from './NoName';
-import Hello from './Hello';
+
 /* JSX 규칙
 여러개의 태그를 사용하려면 그것들을 하나의 루트로 감싸야 함.
 React Fragement 태그
 복수의 자식요소를 그룹화 하는데 사용되며 불필요한 노드를 DOM에 추가하지 않음.
  빈 다이아몬드로 하면 리액트 프래그먼트 태그로 인식함.
- 리액트에서 닫는 태그가 없는태그들은 (/)슬래시를 치고 닫아준다. */ 
-
-const App=()=>{
+ 리액트에서 닫는 태그가 없는태그들은 (/)슬래시를 치고 닫아준다. */
+// props <ExpenseItem title="샤인머스캣"/> APP에 의해 ExpenseItem 에 샤인머스캣이 전달됨
+//  props는 객체이기 때문에 여러개가 전달 가능함.
+// 문자와 다른 데이터는 중괄로로 감싸서 전달하기.
+const App = () => {
+  //지출 항목에 대한 객체배열->손쉽게 관리 가능
+  const expenses = [
+    {
+      title: '바나나나',
+      price: 2000,
+      date: new Date(2023, 4, 23),
+    },
+    {
+      title: '딸기',
+      price: 5000,
+      date: new Date(2023, 6, 23),
+    },
+    {
+      title: '오렌지',
+      price: 7000,
+      date: new Date(2023, 4, 27),
+    },
+  ];
   return (
     <>
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
     </>
   );
 };
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,38 +1,38 @@
-import React from 'react'
+import React from 'react';
 // css 로딩
-import'./ExpenseItem.css';
-const ExpenseItem = () => {
-	const expenseDate=new Date(2023,8,10); //new Date(2023,8,10);
-	const expenseTitle='냠냠치킨';
-	const expensePrice=19000;
+import './ExpenseItem.css';
+// props = property의 약자
+//props는 객체-> 디스트럭쳐링 가능 {title,price,date}
+const ExpenseItem = ({ title, price: propsPrice, date }) => {
+  //객체 형태 {title:'샤인머스캣'}으로 받음.
+  // const expenseDate=date; //new Date(2023,8,10);
+  // const expenseTitle=title;
+  // const expensePrice=propsPrice;//price:propsPrice 별명으로 쓸수 있음.
 
-	//1자리 숫자를 2자리 수로 변환하는 함수
-	const make2digit=(text)=>{
-		return text.toString().padStart(2,'0');
-	}
+  //1자리 숫자를 2자리 수로 변환하는 함수
+  const make2digit = (text) => {
+    return text.toString().padStart(2, '0');
+  };
 
-	//날짜 포맷팅 변환 함수 정의
-	const makeFormattedDate=()=>{
-		const year=expenseDate.getFullYear();
-		const month=expenseDate.getMonth();
-		const date=expenseDate.getDate();
-		return `${year}년 ${make2digit(month)}월 ${make2digit(date)}일`;
-	}
-	//숫자를 원화 표기법으로 바꾸기
-	const formattedPrice=new Intl.NumberFormat('ko-KR').format(expensePrice);
-
-
+  //날짜 포맷팅 변환 함수 정의
+  const makeFormattedDate = () => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
+  };
+  //숫자를 원화 표기법으로 바꾸기
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(propsPrice);
 
   return (
     <div className="expense-item">
       <div>{makeFormattedDate()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
       </div>
     </div>
-   
   );
 };
 
-export default ExpenseItem
+export default ExpenseItem;
