4a978a1160dcaba1df7852bfb2802fc0909b659b
seon <sunkyoung.dev@gmail.com>
2023-09-01 16:55:48 +0900

    동적 랜더링 데이터 및 Date 관련 함수

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>리액트 초보자 입니다</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi React
-        </a>
-      </header>
-    </div>
+    <>
+      <ExpenseItem/>
+      <ExpenseItem/>
+      <ExpenseItem/>
+    </>
   );
 }
 
